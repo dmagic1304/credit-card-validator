a596eea21c20591c2b999f160adc91d23bcc2d1d
Dominik Magic <dominikmagic5@gmail.com>
2022-10-27 11:03:33 -0700

    it will multiplay every other number by 2, starting from right to left
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,19 @@ function isEmpty(testString) {
   return (testString.trim().length === 0);
 }
 
+function onlyNumbers(testString) {
+  return /^[0-9]+$/.test(testString);
+}
+
 function checkAllNumbers (inputNumbers) {
-  if (isEmpty(inputNumbers)) {
+  if (isEmpty(inputNumbers) || !onlyNumbers(inputNumbers)) {
+    console.log("string has characters");
     return 0;
   }
   const creditCardNumbers = inputNumbers.trim().split("");
-  console.log(creditCardNumbers);
-  if ()
+  //console.log(creditCardNumbers.length);
+    for (index = (creditCardNumbers.length - 1); index >= 0 ; index -= 2) {
+      let newNumber = creditCardNumbers[index] * 2;
+      console.log(newNumber);
+    }
 }
