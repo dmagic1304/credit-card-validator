0767cdbf472b69d7c64c94a1a33c20c2640d142e
Dominik Magic <dominikmagic5@gmail.com>
2022-10-27 11:40:11 -0700

    it will add double digit numbers as a single number
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,17 @@ function checkAllNumbers (inputNumbers) {
   }
   const creditCardNumbers = inputNumbers.trim().split("");
   //console.log(creditCardNumbers.length);
-    for (index = (creditCardNumbers.length - 1); index >= 0 ; index -= 2) {
+    for (index = (creditCardNumbers.length - 1); index >= 0 ; index -= 2) {      
       let newNumber = creditCardNumbers[index] * 2;
-      console.log(newNumber);
+      if (newNumber > 9) {
+        let doubleDigit = String(newNumber).split('').reduce((acc, curr) => {return acc + Number(curr);},0);
+        console.log('double digit result: ' + doubleDigit);
+      }
+      //console.log(newNumber);
     }
 }
+
+// const digitOne = (newNumber[0];
+//   console.log(newNumber[0]);
+//   const digitTwo = newNumber[1];
+//   console.log(newNumber[1]);
